714f673b494a1da9d36b88a9f0681d87477090e8
nandiniMora <nandini.mora@vinsys.com>
2021-09-22 20:49:59 +0530

    my maven project

---
+++ src/test/java/mavenProgram/Demo.java
@@ -0,0 +1,17 @@
+package mavenProgram;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("https://opensource-demo.orangehrmlive.com/");
+        
+
+	}
+
+}
